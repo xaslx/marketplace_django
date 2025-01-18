a94c7e6ab4be02dc7c931dc078255811245570ca
maksim <knyazev.maksim2852@gmail.com>
2025-01-18 17:36:27 +0400

    add new endpoint , add query

--- core/api/v1/products/handlers.py
+++ core/api/v1/products/handlers.py
@@ -1,14 +1,42 @@
-from ninja import Router
+from ninja import Query, Router
 from django.http import HttpRequest
-from core.api.v1.products.schemas import ProductListSchema
-
-
+from core.api.filters import PaginationIn, PaginationOut
+from core.api.v1.products.filters import ProductFilters
+from core.api.v1.products.schemas import ProductSchema
+from core.apps.products.entities.products import Product
+from core.apps.products.services.products import BaseProductService, ORMProductService
+from core.api.schemas import ApiResponse, ListPaginationResponse
 
 router: Router = Router(
     tags=['Products'],
 )
 
 
-@router.get('', response=ProductListSchema)
-def get_product_list_handler(request: HttpRequest):
-    return []
+@router.get(
+        '', 
+        response=ApiResponse[ListPaginationResponse[ProductSchema]],
+)
+def get_product_list_handler(
+    request: HttpRequest,
+    filters: Query[ProductFilters],
+    pagination_in: Query[PaginationIn],
+
+):
+    service: BaseProductService = ORMProductService()
+    product_list: list[Product] = service.get_product_list(filters=filters, pagination=pagination_in)
+    product_count: int = service.get_product_count(filters=filters)
+    pagination_out: PaginationOut = PaginationOut(
+            offset=pagination_in.offset, 
+            limit=pagination_in.limit,
+            total=product_count,
+    )
+    items = [ProductSchema.from_entity(entity=obj) for obj in product_list]
+    return ApiResponse(data=ListPaginationResponse(items=items, pagination=pagination_out))
+
+
+
+@router.get('{product_id}', response=ProductSchema)
+def get_product_by_id(request: HttpRequest, product_id: int):
+    service: BaseProductService = ORMProductService()
+    product: Product = service.get_product_by_id(id=product_id)
+    return ProductSchema.from_entity(entity=product)
