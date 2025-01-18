9c253a8918e04c770a1e7b0633c8cd8de9e354f3
maksim <knyazev.maksim2852@gmail.com>
2025-01-18 17:39:40 +0400

    add new method - from_entity

--- core/api/v1/products/schemas.py
+++ core/api/v1/products/schemas.py
@@ -1,14 +1,24 @@
 from pydantic import BaseModel
 from datetime import datetime
 
+from core.apps.products.entities.products import Product
+
 
 
 
 class ProductSchema(BaseModel):
+    id: int
     title: str
     description: str
     created_at: datetime
-    updated_at: datetime
+    updated_at: datetime | None = None
 
-
-ProductListSchema: list[ProductSchema] = list[ProductSchema]
+    @staticmethod
+    def from_entity(entity: Product) -> 'ProductSchema':
+        return ProductSchema(
+            id=entity.id,
+            title=entity.title,
+            description=entity.description,
+            created_at=entity.created_at,
+            updated_at=entity.updated_at,
+        )
