602bf1d1e988703f31da6c0a7f1576a0d747dd99
maksim <knyazev.maksim2852@gmail.com>
2025-01-18 17:38:53 +0400

    add response schemas

--- core/api/schemas.py
+++ core/api/schemas.py
@@ -1,5 +1,28 @@
-from pydantic import BaseModel
+from pydantic import Field
+
+from core.api.filters import PaginationOut
+
+from typing import Any, Generic, TypeVar
+from ninja import Schema
 
 
-class PingResponseSchema(BaseModel):
+
+class PingResponseSchema(Schema):
     result: bool
+
+
+TData = TypeVar('TData')
+TListItem = TypeVar('TListItem')
+
+
+
+class ListPaginationResponse(Schema, Generic[TListItem]):
+    items: list[TListItem]
+    pagination: PaginationOut
+
+class ApiResponse(Schema, Generic[TData]):
+    data: TData | dict = Field(default_factory=dict)
+    meta: dict[str, Any] = Field(default_factory=dict)
+    errors: list[Any] = Field(default_factory=list)
+
+
