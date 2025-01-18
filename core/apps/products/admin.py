44762b6539ed462c6a6321b285ac369b01e59998
maksim <knyazev.maksim2852@gmail.com>
2025-01-18 13:07:55 +0400

    register model in admin

--- core/apps/products/admin.py
+++ core/apps/products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.apps.products.models.products import Product
 
-# Register your models here.
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at')
--- core/apps/products/apps.py
+++ core/apps/products/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class ProductsConfig(AppConfig):
+class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'products'
+    name = 'core.apps.products'
+    verbose_name = 'Товары'
