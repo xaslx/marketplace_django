ab2d67e87cf03d7891f845ad839e87fac37e5e46
maksim <knyazev.maksim2852@gmail.com>
2025-01-20 12:24:57 +0400

    add tests

--- tests/factories/products.py
+++ tests/factories/products.py
@@ -1,5 +1,11 @@
 from factory.django import DjangoModelFactory
 import factory
+from core.apps.products.models.products import Product
+
 
 class ProductModelFactory(DjangoModelFactory):
-    ...
+    title = factory.Faker('first_name')
+    description = factory.Faker('text')
+
+    class Meta:
+        model = Product
--- tests/services/test_products.py
+++ tests/services/test_products.py
@@ -1,7 +1,9 @@
+from core.api.filters import PaginationIn
 from core.api.v1.products.filters import ProductFilters
 from core.apps.products.services.products import BaseProductService
 import pytest
 from core.apps.products.models.products import Product
+from tests.factories.products import ProductModelFactory
 
 @pytest.mark.django_db
 def test_products_count_zero(product_service: BaseProductService):
@@ -14,11 +16,29 @@ def test_products_count_zero(product_service: BaseProductService):
 @pytest.mark.django_db
 def test_products_count_not_zero(product_service: BaseProductService):
     """Тест с количеством товаров больше 0"""
+    
+    expected_count: int = 5
+    ProductModelFactory.create_batch(size=expected_count)
 
-    Product.objects.create()
+    products_count: int = product_service.get_product_count(filters=ProductFilters())
+    assert products_count == expected_count, f'{products_count=}'
 
 
+@pytest.mark.django_db
+def test_get_product_list(product_service: BaseProductService):
+    """Тест на получение списка продуктов"""
 
+    expected_count: int = 5
+    products = ProductModelFactory.create_batch(size=expected_count)
+    product_titles = {product.title for product in products}
+
+    fetched_products = product_service.get_product_list(filters=ProductFilters(), pagination=PaginationIn())
+    fetched_titles = {product.title for product in fetched_products}
+
+    assert len(fetched_titles) == expected_count, f'{fetched_titles=}'
+    assert product_titles == fetched_titles
+
+    
 @pytest.mark.skip(reason='not implemented')
 def test_product_search():
     assert True
