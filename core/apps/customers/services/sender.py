d14503e1e9ed6c80b1636905d17587b0f2d32069
maksim <knyazev.maksim2852@gmail.com>
2025-01-20 21:34:25 +0400

    add ConsoleSenderService

--- core/apps/customers/services/sender.py
+++ core/apps/customers/services/sender.py
@@ -1,10 +1,16 @@
 from abc import ABC, abstractmethod
 
+from core.apps.customers.entities import CustomerEntity
+
 
 class BaseSenderService(ABC):
 
     @abstractmethod
-    def send_code(self, code: str) -> None:
+    def send_code(self, customer: CustomerEntity, code: str) -> None:
         ...
 
 
+class ConsoleSenderService(BaseSenderService):
+
+    def send_code(self, customer: CustomerEntity, code: str) -> None:
+        print(f'Code to user: {customer.phone}, sent: {code}')
