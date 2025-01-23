af2b3bf483154afd6885f0e40e9bb8bf38391db5
maksim <knyazev.maksim2852@gmail.com>
2025-01-23 19:08:28 +0400

    fix

--- core/apps/products/services/products.py
+++ core/apps/products/services/products.py
@@ -2,56 +2,54 @@ from abc import ABC, abstractmethod
 from typing import Iterable
 
 from django.db.models import Q
-
-from core.api.v1.products.filters import ProductFilters
+from core.apps.products.filters.products import ProductFilters
 from core.api.filters import PaginationIn
-from core.apps.products.entities.products import Product
+from core.apps.products.entities.products import ProductEntity
 from core.apps.products.models.products import Product as ProductModel
 
 
 class BaseProductService(ABC):
-    
     @abstractmethod
-    def get_product_list(self, filters: ProductFilters, pagination: PaginationIn) -> Iterable[Product]:
+    def get_product_list(
+            self,
+            filters: ProductFilters,
+            pagination: PaginationIn,
+    ) -> Iterable[ProductEntity]:
         ...
-
 
     @abstractmethod
     def get_product_count(self, filters: ProductFilters) -> int:
         ...
 
-
     @abstractmethod
-    def get_product_by_id(self, id: int) -> Product:
+    def get_product_by_id(self, id: int) -> ProductEntity:
         ...
 
 
 class ORMProductService(BaseProductService):
-    
     def _build_product_query(self, filters: ProductFilters) -> Q:
         query = Q(is_visible=True)
 
         if filters.search is not None:
             query &= (Q(title__icontains=filters.search) | Q(description__icontains=filters.search))
-        
         return query
 
-    def get_product_list(self, filters: ProductFilters, pagination: PaginationIn) -> Iterable[Product]:
-        
+    def get_product_list(
+            self,
+            filters: ProductFilters,
+            pagination: PaginationIn,
+    ) -> Iterable[ProductEntity]:
         query = self._build_product_query(filters=filters)
 
-        qs = ProductModel.objects.filter(query)[pagination.offset:pagination.offset + pagination.limit]
+        qs = ProductModel.objects.filter(query)[
+            pagination.offset:pagination.offset + pagination.limit
+        ]
         return [product.to_entity() for product in qs]
 
-
     def get_product_count(self, filters: ProductFilters) -> int:
-        
         query = self._build_product_query(filters=filters)
         return ProductModel.objects.filter(query).count()
-    
 
-    def get_product_by_id(self, id: int) -> Product:
-        
+    def get_product_by_id(self, id: int) -> ProductEntity:
         qs = ProductModel.objects.filter(is_visible=True).get(pk=id)
         return ProductModel.to_entity(qs)
-
