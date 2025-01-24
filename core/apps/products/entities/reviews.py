1c3ff79f73fc553181a9a39c5a56a3e0ec596a3f
maksim <knyazev.maksim2852@gmail.com>
2025-01-24 12:37:40 +0400

    add new field to Review Entity

--- core/apps/products/entities/reviews.py
+++ core/apps/products/entities/reviews.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from datetime import datetime
 from core.apps.common.enums import EntityStatus
 from core.apps.products.entities.products import ProductEntity
 from core.apps.customers.entities import CustomerEntity
@@ -6,7 +7,10 @@ from core.apps.customers.entities import CustomerEntity
 
 @dataclass
 class ReviewEntity:
+    id: int | None = field(default=None, kw_only=True)
     customer: CustomerEntity | EntityStatus = field(default=EntityStatus.NOT_LOADED)
     product: ProductEntity | EntityStatus = field(default=EntityStatus.NOT_LOADED)
     text: str = field(default='')
     rating: int = field(default=1)
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime | None = field(default=None)
