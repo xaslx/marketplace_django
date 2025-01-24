141525f5c6e2da2ad1fa9dffd1c8b42ad9c49a82
maksim <knyazev.maksim2852@gmail.com>
2025-01-24 12:40:55 +0400

    add new method - get_by_token

--- core/apps/customers/services/customers.py
+++ core/apps/customers/services/customers.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from uuid import uuid4
 
 from core.apps.customers.entities import CustomerEntity
+from core.apps.customers.exceptions.customers import CustomerTokenInvalidException
 from core.apps.customers.models import Customer
 
 
@@ -18,18 +19,22 @@ class BaseCustomerService(ABC):
     @abstractmethod
     def get(self, phone: str) -> CustomerEntity:
         ...
+    
+    @abstractmethod
+    def get_by_token(self, token: str) -> CustomerEntity | None:
+        ...
 
 
 class ORMCustomerService(BaseCustomerService):
 
     def get_or_create(self, phone: str) -> CustomerEntity:
-        user_dto: Customer
-        user_dto, _ = Customer.objects.get_or_create(phone=phone)
-        return user_dto.to_entity()
+        customer_dto: Customer
+        customer_dto, _ = Customer.objects.get_or_create(phone=phone)
+        return customer_dto.to_entity()
     
     def get(self, phone: str) -> CustomerEntity:
-        user_dto: Customer = Customer.objects.get(phone=phone)
-        return user_dto.to_entity()
+        customer_dto: Customer = Customer.objects.get(phone=phone)
+        return customer_dto.to_entity()
     
     def generate_token(self, customer: CustomerEntity) -> str:
         new_token: str = str(uuid4())
@@ -37,3 +42,11 @@ class ORMCustomerService(BaseCustomerService):
             token=new_token,
         )
         return new_token
+    
+    def get_by_token(self, token: str) -> CustomerEntity | None:
+        try:
+            customer_dto: Customer = Customer.objects.get(token=token)
+        except Customer.DoesNotExist:
+            raise CustomerTokenInvalidException(token=token)
+        
+        return customer_dto.to_entity()
