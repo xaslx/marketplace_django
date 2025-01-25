b0719a17ad292cec14a92ea039204829a85a29ee
maksim <knyazev.maksim2852@gmail.com>
2025-01-25 11:40:48 +0400

    add sender services

--- core/apps/customers/services/sender.py
+++ core/apps/customers/services/sender.py
@@ -1,15 +1,38 @@
-from abc import ABC, abstractmethod
+from abc import (
+    ABC,
+    abstractmethod,
+)
+from dataclasses import dataclass
+from typing import Iterable
+
 from core.apps.customers.entities import CustomerEntity
 
 
 class BaseSenderService(ABC):
-
     @abstractmethod
     def send_code(self, customer: CustomerEntity, code: str) -> None:
         ...
 
 
 class ConsoleSenderService(BaseSenderService):
+    def send_code(self, customer: CustomerEntity, code: str) -> None:
+        print(f'Code to user: {customer}, sent: {code}')
+
+
+class EmailSenderService(BaseSenderService):
+    def send_code(self, customer: CustomerEntity, code: str) -> None:
+        print(f'sent code {code} to user email: customeremail')
+
+
+class PushSenderService(BaseSenderService):
+    def send_code(self, customer: CustomerEntity, code: str) -> None:
+        print(f'sent push notification with {code} fcm_token')
+
+
+@dataclass
+class ComposedSenderService(BaseSenderService):
+    sender_services: Iterable[BaseSenderService]
 
     def send_code(self, customer: CustomerEntity, code: str) -> None:
-        print(f'Code to user: {customer.phone}, sent: {code}')
+        for service in self.sender_services:
+            service.send_code(customer, code)
